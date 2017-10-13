c90017a46ed3b12f45ed9f8f28cda747d634ea6a
Arczi14 <arekc247@gmail.com>
2017-10-13 21:38:14 +0200

    Perfekt-Bud 12.10.2017

---
+++ js/scroll-nav.js
@@ -0,0 +1,21 @@
+	//------------------------------------------------------
+	//podczas scrollu dodaje headerowi klase hover
+	//------------------------------------------------------
+
+	document.addEventListener('DOMContentLoaded', function() {
+
+        let testNav = function() {
+            var top = (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0);
+            if (top > 155) {
+                nav.classList.add('nav-scroll');
+            } else {
+                nav.classList.remove('nav-scroll');
+            }
+        };
+
+		let nav = document.querySelector('.main-nav');
+		testNav();
+		window.addEventListener('scroll', function() {
+			testNav();
+		})
+	});
