4684590022d27c8a43084f0dcb9cf54bc7622622
Arczi14 <arekc247@gmail.com>
2017-10-18 16:27:08 +0200

    slideToggle()

--- js/readMore.js
+++ js/readMore.js
@@ -1,13 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
 
-let btn = document.querySelector('.myBtn');
-let offer = document.querySelector('.my_offer');
-let parentBtn = document.querySelector('.parentBtn')
+// let btn = document.querySelector('.myBtn');
+// let offer = document.querySelector('.my_offer');
+// let parentBtn = document.querySelector('.parentBtn')
 
-    btn.addEventListener('click', function() {
-        offer.style.display="block";
-        btn.innerText="Zwiń";
-        parentBtn.style.marginTop='50px';
+//     btn.addEventListener('click', function() {
+//         offer.slideToggle(1000);
+//         btn.innerText="Zwiń";
+//         parentBtn.style.marginTop='50px';
+//     })
+
+// });
+
+$(function(){
+  
+    let btn = $('.myBtn');
+    let offer = $('.my_offer');
+    let parentBtn = $('.parentBtn')
+
+    
+    offer.hide();
+    btn.on('click', function(){
+
+        if ( $(this).text() === 'Zobacz więcej'){
+            $(this).text('Zwiń');
+        } else {
+            $(this).text('Zobacz więcej');
+        }
+
+        offer.slideToggle(1000);
+        parentBtn.css('marginTop', '50px');
     })
-
+    
 });
+    
