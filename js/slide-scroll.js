2b23ccadaf6fbddc67bb1943b52d5eff3efaefe1
Arczi14 <arekc247@gmail.com>
2018-01-20 20:32:13 +0100

    v2

--- js/slide-scroll.js
+++ js/slide-scroll.js
@@ -1,44 +1,44 @@
 $(function() {
-                
-    var $mainNav = $('.main-nav');
-    var $window = $(window);
-    var $sections = [];
     
-    $mainNav.find('a').each(function() {
-         $sections.push($($(this).attr('href')));
-    })
-    
-    var autoAddClassOnScroll = function () {
-        //automatyczne dodawanie klasy przy scrollowaniu
-        //jak nie potrzeba to usun ponizsze linie
-        let active = -1;
-        $.each($sections, function(i) {          
-            if ($sections[i].offset().top <= $window.scrollTop() + $window.height()/5) {                                
-                active = i;
-            }            
-        })
-        if (active >= 0) {
-            $mainNav.find('li').removeClass('active');
-            $mainNav.find('li').eq(active).addClass('active');
-        }
-    }    
+var $mainNav = $('.main-nav');
+var $window = $(window);
+var $sections = [];
 
-    $mainNav.find('a').on('click', function(e) {
-        e.preventDefault();
-        var href = $(this).attr('href');
-        var $target = $(href);
+$mainNav.find('a').each(function() {
+$sections.push($($(this).attr('href')));
+})
 
-        $(this).parent().siblings().removeClass('active');
-        $(this).parent().addClass('active');
+var autoAddClassOnScroll = function () {
+//automatyczne dodawanie klasy przy scrollowaniu
+//jak nie potrzeba to usun ponizsze linie
+let active = -1;
+$.each($sections, function(i) {          
+if ($sections[i].offset().top <= $window.scrollTop() + $window.height()/5) {                                
+    active = i;
+}            
+})
+if (active >= 0) {
+$mainNav.find('li').removeClass('active');
+$mainNav.find('li').eq(active).addClass('active');
+}
+}    
 
-        $('html, body').animate({
-            scrollTop : $target.offset().top
-        }, 1000)
-    });
-    $window.on('scroll', function() {
-        autoAddClassOnScroll();
-    });
+$mainNav.find('a').on('click', function(e) {
+e.preventDefault();
+var href = $(this).attr('href');
+var $target = $(href);
 
-    autoAddClassOnScroll();
+$(this).parent().siblings().removeClass('active');
+$(this).parent().addClass('active');
+
+$('html, body').animate({
+scrollTop : $target.offset().top
+}, 1000)
+});
+$window.on('scroll', function() {
+autoAddClassOnScroll();
+});
+
+autoAddClassOnScroll();
 
 })
