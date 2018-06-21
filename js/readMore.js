ce3fb02855d15ff1a4e34acb231cad55efc235eb
Ścibior Arkadiusz <ascibior@kbj.com.pl>
2018-06-21 14:26:08 +0200

    poprawa organizacji kodu w plikach

--- js/readMore.js
+++ js/readMore.js
@@ -1,2 +1,15 @@
-$(function(){let btn=$('.myBtn');let offer=$('.my_offer');let parentBtn=$('.parentBtn')
-offer.hide();btn.on('click',function(){if($(this).text()==='Zobacz więcej'){$(this).text('Zwiń');}else{$(this).text('Zobacz więcej');}offer.slideToggle(1000);parentBtn.css('marginTop','50px');})});
+$(function () {
+    let btn = $('.myBtn');
+    let offer = $('.my_offer');
+    let parentBtn = $('.parentBtn')
+    offer.hide();
+    btn.on('click', function () {
+        if ($(this).text() === 'Zobacz więcej') {
+            $(this).text('Zwiń');
+        } else {
+            $(this).text('Zobacz więcej');
+        }
+        offer.slideToggle(1000);
+        parentBtn.css('marginTop', '50px');
+    })
+});
